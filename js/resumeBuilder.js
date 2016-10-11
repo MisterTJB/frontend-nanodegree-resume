89fa595e81fa65480376f5dbd0c8ec431db9ab48
Tim <tim.bathgate@gmail.com>
2016-10-12 11:14:08 +1300

    Beautify JS

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,90 +1,77 @@
 "use strict";
 
 var work = {
-    jobs: [
-        {
-            employer: "VicLink / Ryan Achten",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        },
-        {
-            employer: "Victoria University of Wellington",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        },
-        {
-            employer: "Victoria University of Wellington",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        },
-        {
-            employer: "National Archive of Film Television and Sound",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        },
-        {
-            employer: "The British Library",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        },
-        {
-            employer: "Radio New Zealand Sound Archives",
-            title: "yyy",
-            location: "zzz",
-            dates: "###-###",
-            description: "dslgksladg lsdjglasngd lkdgalsdkgj"
-        }
-    ],
-    display: function () {
+    jobs: [{
+        employer: "VicLink / Ryan Achten",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }, {
+        employer: "Victoria University of Wellington",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }, {
+        employer: "Victoria University of Wellington",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }, {
+        employer: "National Archive of Film Television and Sound",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }, {
+        employer: "The British Library",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }, {
+        employer: "Radio New Zealand Sound Archives",
+        title: "yyy",
+        location: "zzz",
+        dates: "###-###",
+        description: "dslgksladg lsdjglasngd lkdgalsdkgj"
+    }],
+    display: function() {
         return 0;
     }
 };
 
 
 var projects = {
-    projects: [
-        {
-            title: "xxx",
-            dates: "###-###",
-            description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
-            images: ["/path/to/image", "path/to/image_2"]
-        },
-        {
-            title: "xxx",
-            dates: "###-###",
-            description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
-            images: ["/path/to/image", "path/to/image_2"]
-        },
-        {
-            title: "xxx",
-            dates: "###-###",
-            description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
-            images: ["/path/to/image", "path/to/image_2"]
-        },
-        {
-            title: "xxx",
-            dates: "###-###",
-            description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
-            images: ["/path/to/image", "path/to/image_2"]
-        },
-        {
-            title: "xxx",
-            dates: "###-###",
-            description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
-            images: ["/path/to/image", "path/to/image_2"]
-        }
-    ],
-    display: function () {
+    projects: [{
+        title: "xxx",
+        dates: "###-###",
+        description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
+        images: ["/path/to/image", "path/to/image_2"]
+    }, {
+        title: "xxx",
+        dates: "###-###",
+        description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
+        images: ["/path/to/image", "path/to/image_2"]
+    }, {
+        title: "xxx",
+        dates: "###-###",
+        description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
+        images: ["/path/to/image", "path/to/image_2"]
+    }, {
+        title: "xxx",
+        dates: "###-###",
+        description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
+        images: ["/path/to/image", "path/to/image_2"]
+    }, {
+        title: "xxx",
+        dates: "###-###",
+        description: "sdlkshdg sdlgjsdgljk sdlgjksdg",
+        images: ["/path/to/image", "path/to/image_2"]
+    }],
+    display: function() {
         return 0;
     }
 };
@@ -105,68 +92,59 @@ var bio = {
     skills: [
         "iOS", "Web", "Data analysis", "Flying"
     ],
-    display: function () {
+    display: function() {
         return 0;
     }
 };
 
 var education = {
-    schools: [
-        {
-            name: "Victoria University of Wellington",
-            location: "Wellington",
-            degree: "XYZ",
-            dates: "2015-2016",
-            url: "www.victoria.ac.nz",
-            majors: ["Computer Science"]
-        },
-        {
-            name: "University of Canterbury",
-            location: "Wellington",
-            degree: "XYZ",
-            dates: "2010-2014",
-            url: "www.canterbury.ac.nz",
-            majors: ["Computer Science", "Mathematics"]
-        },
-        {
-            name: "Victoria University of Wellington",
-            location: "Wellington",
-            degree: "XYZ",
-            dates: "2008-2010",
-            url: "www.victoria.ac.nz",
-            majors: ["Information Studies"]
-        },
-        {
-            name: "Tai Poutini Polytechnic",
-            location: "Christchurch",
-            degree: "XYZ",
-            dates: "2004-2005",
-            url: "www.mainz.ac.nz",
-            majors: ["Audio Engineering"]
-        }
-    ],
+    schools: [{
+        name: "Victoria University of Wellington",
+        location: "Wellington",
+        degree: "XYZ",
+        dates: "2015-2016",
+        url: "www.victoria.ac.nz",
+        majors: ["Computer Science"]
+    }, {
+        name: "University of Canterbury",
+        location: "Wellington",
+        degree: "XYZ",
+        dates: "2010-2014",
+        url: "www.canterbury.ac.nz",
+        majors: ["Computer Science", "Mathematics"]
+    }, {
+        name: "Victoria University of Wellington",
+        location: "Wellington",
+        degree: "XYZ",
+        dates: "2008-2010",
+        url: "www.victoria.ac.nz",
+        majors: ["Information Studies"]
+    }, {
+        name: "Tai Poutini Polytechnic",
+        location: "Christchurch",
+        degree: "XYZ",
+        dates: "2004-2005",
+        url: "www.mainz.ac.nz",
+        majors: ["Audio Engineering"]
+    }],
 
-    onlineCourses: [
-        {
-            title: "Front-end Web Developer Nanodegree",
-            school: "Udacity",
-            dates: "2016",
-            url: "www.udacity.com"
-        },
-        {
-            title: "iOS Developer Nanodegree",
-            school: "Udacity",
-            dates: "2015-2016",
-            url: "www.udacity.com"
-        },
-        {
-            title: "Data Analyst Developer Nanodegree",
-            school: "Udacity",
-            dates: "2015-2016",
-            url: "www.udacity.com"
-        }
-    ],
-    display: function () {
+    onlineCourses: [{
+        title: "Front-end Web Developer Nanodegree",
+        school: "Udacity",
+        dates: "2016",
+        url: "www.udacity.com"
+    }, {
+        title: "iOS Developer Nanodegree",
+        school: "Udacity",
+        dates: "2015-2016",
+        url: "www.udacity.com"
+    }, {
+        title: "Data Analyst Developer Nanodegree",
+        school: "Udacity",
+        dates: "2015-2016",
+        url: "www.udacity.com"
+    }],
+    display: function() {
         return 0;
     }
 };
